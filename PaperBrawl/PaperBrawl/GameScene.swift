d53c5f8f2339c0b954a9d1c154aa871d79f4a6f7
Jonathan Newberry <jdnewberry@smartronix.com>
2016-07-11 20:43:13 -0400

    Added preliminary graphics to the project as well as the GameScene.

--- PaperBrawl/PaperBrawl/GameScene.swift
+++ PaperBrawl/PaperBrawl/GameScene.swift
@@ -11,8 +11,8 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
+        let myLabel = SKLabelNode(fontNamed:"Helvetica Nue")
+        myLabel.text = "Paper Brawl"
         myLabel.fontSize = 45
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
