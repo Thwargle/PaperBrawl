261b7c01373d26f96ab6c93b943a68271e0647da
Thwargle <jdnewberry@smartronix.com>
2016-07-19 23:16:04 -0400

    Initial movement implementation. Character moves and jumps around the stage and collides with objects.

--- PaperBrawl/PaperBrawl/GameScene.swift
+++ PaperBrawl/PaperBrawl/GameScene.swift
@@ -8,6 +8,8 @@
 
 import SpriteKit
 
+let movePlayerRightButton = "playerRight"
+
 class GameScene: SKScene {
     
     let playerLeft = SKSpriteNode(imageNamed: "arrowLeft")
@@ -15,27 +17,82 @@ class GameScene: SKScene {
     let playerUp = SKSpriteNode(imageNamed: "arrowUp")
     let playerDown = SKSpriteNode(imageNamed: "arrowDown")
     
+    var isFingerOnPlayerUp = false
+    var isFingerOnPlayerDown = false
+    var isFingerOnPlayerLeft = false
+    var isFingerOnPlayerRight = false
+    
+    var frameCount = 0
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Helvetica Nue")
-        myLabel.text = "Paper Brawl"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
         
-        self.addChild(myLabel)
+        borderBody.friction = 0
+        
+        self.physicsBody = borderBody
+        
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -9.8)
     }
     
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        for touch: AnyObject     in touches {
-            let location = touch.locationInNode(self)
-            if playerUp.containsPoint(location) {
-                print("Move up. Location: \(location)")
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first
+        let touchLocation = touch!.locationInNode(self)
+        if let body = physicsWorld.bodyAtPoint(touchLocation) {
+            if body.node!.name == "playerUp" {
+                isFingerOnPlayerUp = true
+            }
+            if body.node!.name == "playerDown" {
+                isFingerOnPlayerDown = true
+            }
+            if body.node!.name == "playerLeft" {
+                isFingerOnPlayerLeft = true
+            }
+            if body.node!.name == "playerRight" {
+                isFingerOnPlayerRight = true
             }
         }
     }
     
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        isFingerOnPlayerUp = false
+        isFingerOnPlayerDown = false
+        isFingerOnPlayerLeft = false
+        isFingerOnPlayerRight = false
+    }
     
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        if(isFingerOnPlayerLeft) {
+            let player = childNodeWithName("playerSprite") as! SKSpriteNode
+            
+            let playerX = player.position.x - 15
+            
+            player.position = CGPoint(x: playerX, y: player.position.y)
+        }
+        if(isFingerOnPlayerRight) {
+            let player = childNodeWithName("playerSprite") as! SKSpriteNode
+            
+            let playerX = player.position.x + 15
+            
+            player.position = CGPoint(x: playerX, y: player.position.y)
+        }
+        if(isFingerOnPlayerUp) {
+            let player = childNodeWithName("playerSprite") as! SKSpriteNode
+            if(frameCount < 8) {
+                if(isFingerOnPlayerLeft) {
+                    player.physicsBody!.applyImpulse(CGVector(dx: -1, dy: 40.0))
+                }
+                if(isFingerOnPlayerRight) {
+                    player.physicsBody!.applyImpulse(CGVector(dx: 1, dy: 40.0))
+                }
+                else {
+                    player.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 40.0))
+                }
+                frameCount += 1
+            }
+            else {
+                frameCount = 0
+                isFingerOnPlayerUp = false
+            }
+        }
     }
 }
